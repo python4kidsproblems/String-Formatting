3c100799c8c5e65d6f239adf8bec6f2d7e539f9d
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-09-02 01:27:38 +0500

    Add files via upload

---
+++ String_formatting.py
@@ -0,0 +1,45 @@
+#String Formatting Practice
+
+# How to use this file:
+#  Copy this to your python (.py) file, and write your code in the given space
+
+#Question 1 and 2 below have some variables given which you need to format into presentable output strings.
+
+# 1. first_name = "Eric"
+# 	 last_name = "Idle"
+# 	 age = 74
+# 	 profession = "comedian"
+# 	 affiliation = "Monty Python"
+
+# 	 Output: Hello, Eric Idle. You are 74. You are a comedian. You were a member of Monty Python.
+
+
+# your code goes here
+
+# 2. Let's try on with a dictionary:
+#    comedian = {'name': 'Eric Idle', 'age': 74}
+
+#    Output: The comedian is Eric Idle, aged 74.
+
+
+# your code goes here
+
+
+#For the next 4 questions guess the output of the blocks of code:
+
+# 3. output = '{:d}'.format(24)
+#    print(output)
+
+# 4. output = '{:f}'.format(5.12345678123)
+#    print(output)
+
+# 5. data = {'first': 'Place', 'last': 'Holder!'}
+#    output = '{first} {last}'.format(**data)
+#    print(output)
+
+# 6. from datetime import datetime
+#    output='{:%Y-%m-%d %H:%M}'.format(datetime(2016, 7, 26, 3, 57))
+#    print(output)
+
+
+#Check your guesses by following this link: https://www.w3resource.com/python/python-format.php
